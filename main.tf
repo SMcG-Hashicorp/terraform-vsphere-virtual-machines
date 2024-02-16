6025702f2258a406a129ac56f944cad30640c904
SMcG-Hashicorp <121955406+SMcG-Hashicorp@users.noreply.github.com>
2024-02-16 15:01:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "vsphere" {
-  user           = "administrator@vsphere.local"
-  password       = "VMware1!"
-  vsphere_server = "vCenter67.vSphere.Lab"
-
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
+
 data "vsphere_datacenter" "dc" {
   name = var.vsphere_datacenter
 }
