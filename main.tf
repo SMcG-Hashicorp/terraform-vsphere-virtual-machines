0f58f00061b20eb100c5fabb85f39ae304492728
SMcG-Hashicorp <121955406+SMcG-Hashicorp@users.noreply.github.com>
2024-02-16 15:03:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
+  vsphere_user                 = var.vsphere_user
+  vsphere_password             = var.vsphere_password
   vsphere_server       = var.vsphere_server
   # If you have a self-signed cert
   allow_unverified_ssl = true
