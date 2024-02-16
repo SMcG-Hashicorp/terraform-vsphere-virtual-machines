29056a1b8894ad9a44799d519b624e095bc2b4c7
SMcG-Hashicorp <121955406+SMcG-Hashicorp@users.noreply.github.com>
2024-02-16 15:00:36 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+variable "vsphere_user" {
+  user = "administrator@vsphere.local"
+  description = "User Logon to vCenter."
+}
+
+variable "vsphere_password" {  
+  password       = "VMware1!"
+  description = "Password - default."
+}
+
+variable "vsphere_server" {   
+  vsphere_server = "vCenter67.vSphere.Lab"
+  description = "vcenter67 server to connect to."
+}
+
 variable "vsphere_datacenter" {
   default = "VM-Lab"
   description = "The datacenter where the virtual machines will be put."
